9b8e3667dbaa92f3bb62596c62f4ae2710c62ded
faustovaz <faustovaz@gmail.com>
2016-10-01 17:28:08 -0300

    Adding a rbenv install script

--- functions.sh
+++ functions.sh
@@ -109,3 +109,8 @@ install_pyenv(){
   git clone https://github.com/yyuu/pyenv.git ~/.pyenv
   git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 }
+
+install_rbenv(){
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+}
